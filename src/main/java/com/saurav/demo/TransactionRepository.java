d85a5466ee88f1f4a4517be1648c7550302141b0
student <student@GBTC441J15LX.montgomerycollege.edu>
2017-06-23 17:38:16 -0400

    added files

--- src/main/java/com/saurav/demo/TransactionRepository.java
+++ src/main/java/com/saurav/demo/TransactionRepository.java
@@ -6,9 +6,13 @@ import org.springframework.data.repository.CrudRepository;
 
 import com.saurav.demo.Transactions;
 
+import antlr.collections.List;
+
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
 public interface TransactionRepository extends CrudRepository<Transactions, Long> {
+	
+	List findByAccountId(long accountId);
 
 }
